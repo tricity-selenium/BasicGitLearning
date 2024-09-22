47639ac5c0dc5d5029ab7253f93b8399b066f250
tricity-selenium <tricity.selenium@gmail.com>
2024-09-22 21:37:41 +0530

    First Commit

---
+++ Addition.java
@@ -0,0 +1,17 @@
+package package1;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 2;
+		int b = 3;
+		int c = a+b;
+		System.out.println("The of numbers is = " + c);
+		
+		
+
+	}
+
+}
